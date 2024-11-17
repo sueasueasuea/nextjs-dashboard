4ca473bf3419a3eb0bb3028b7df3a5e1925435d4
siripol <siripol.th@ku.th>
2024-11-17 22:18:01 +0700

    fix: props updateInv

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -67,7 +67,9 @@ export async function createInvoice(prevState:State,formData: FormData) {
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function updateInvoice(prevState:State,id: string, formData: FormData) {
+export async function updateInvoice(id: string,
+  prevState: State,
+  formData: FormData,) {
 
   const validatedFields  = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
