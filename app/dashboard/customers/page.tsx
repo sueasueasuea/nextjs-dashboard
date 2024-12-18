a1da317b36ab09f342031a227eaa17e9b1b7c493
siripol <siripol.th@ku.th>
2024-12-19 00:57:22 +0700

    feat: complete seo and meta data

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
+import { Metadata } from 'next';
 
+export const metadata: Metadata = {
+  title: 'Customers',
+};
 export default function page() {
   return (
     <div>Customers Page</div>
